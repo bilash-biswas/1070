ea87dcd4744cbdf93bc64e356876d21f001a6b90
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 14:21:33 +0600

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -4,17 +4,17 @@ input = input.replace('\n',' ');
 var lines = input.split(' ');
 var X = parseInt(lines.shift());
 var i;
-if(X%2===0)
+if(X % 2 === 0)
 {
-    for(i=1;i<12;i=i+2)
+    for(i = 1;i < 12;i = i + 2)
     {
-     console.log(X+i);
+     console.log(X + i);
     }
 }
 else
 {
-   for(i=2;i<=12;i+=2)
+   for(i = 2;i <= 12;i += 2)
     {
-     console.log(X+i);
+     console.log(X + i);
     }
 }
